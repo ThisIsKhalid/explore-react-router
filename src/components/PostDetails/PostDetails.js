93eb1b2a54c4c7e293a67c6301ee2ee4c2e7d06f
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-10-05 11:29:41 +0600

    added postdeatails

--- src/components/PostDetails/PostDetails.js
+++ src/components/PostDetails/PostDetails.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const PostDetails = () => {
     const post = useLoaderData();
-    const {id, title, body} = post;
+    const {id, title, body, userId} = post;
+    const navigate = useNavigate();
+    const findAuthor = () => {
+        navigate(`/friend/${userId}`)
+    }
     return (
         <div>
             <h3>ID: {id}</h3>
             <h5>{title}</h5>
             <p>{body}</p>
+            <button onClick={findAuthor}>Get Author</button>
         </div>
     );
 };
